c90ccd70d94e5677d6e7fb646280c5e1a9cdb7e0
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-10-18 16:30:31 +0300

    app structure

--- src/components/board/column/card/card.jsx
+++ src/components/board/column/card/card.jsx
@@ -1,7 +1,10 @@
-export default function Card({props}) {
+import IconCross from "../../../shared/cross";
+
+export default function Card({ props }) {
     return (
         <div className="card">
-            <span className="card__content"></span>
+            <span className="card__content"><button className="button__delete"><IconCross/></button>s</span>
+            
         </div>
     )
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,15 @@
 export default function Footer() {
     return (
-        <footer className="footer">Footer</footer>
+        <footer className="footer">
+            <div className="container footer__content">
+                <div className="taskCounters">
+                    <span className="taskCounter">Active tasks: 0</span>
+                    <span className="taskCounter">Finished tasks: 0</span>
+                </div>
+                <div className="authorName">
+                    <span>Kanban board by Author</span>
+                </div>
+            </div>
+        </footer>
     )
 }
